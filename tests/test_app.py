315f79a6e925db4b282f1a130d2f08999dbddf52
‘AramK0’ <aramk6827@gmail.com>
2025-07-09 18:53:38 +0300

    more ci testing

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,18 +1,21 @@
-from fastapi.testclient import TestClient
-from app import app
+import pytest
+import httpx
 
+base_url = 'http://localhost:8000'
 
-client = TestClient(app)
+def test_home():
+    r = httpx.get(f"{base_url}/")
+    assert r.status_code == 200
+    
+def test_about():
+    r = httpx.get(f"{base_url}/about")
+    assert r.status_code == 200
 
+def test_contactus():
+    r = httpx.get(f"{base_url}/contact")
+    assert r.status_code == 200
 
-def test_homepage():
-    response = client.get("/")
-    assert response.status_code == 200
-
-def test_homepage():
-    response = client.get("/about")
-    assert response.status_code == 200
-
-def test_homepage():
-    response = client.get("/contact")
-    assert response.status_code == 200
+def test_chat_post():
+    r = httpx.post(f"{base_url}/chat", json={"message": "hello"})
+    assert r.status_code == 200
+    return "response" in r.json

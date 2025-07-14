60769892a61e6c93328f118f799ade3aa2ec8d8f
‘AramK0’ <aramk6827@gmail.com>
2025-07-14 21:40:10 +0300

    first setup script

---
+++ scripts/first_setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#run this for first serup when pulled the repo
+
+
+
+#install docker
+sudo apt update && sudo apt upgrade -y
+sudo apt install -y ca-certificates curl gnupg lsb-release
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo service docker start
+docker --version
+
+#for making .env, input your own api keys here
+echo OPENAI_API_KEY = 1234x > .env
+echo ANTHROPIC_API_KEY = 1234x > .env
+
+#temp fix for the perms when running docker
+sudo chmod 777 logs/

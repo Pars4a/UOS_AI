cba1e1ab4120201f41ccd9b186141a9909d795e3
‘AramK0’ <aramk6827@gmail.com>
2025-08-02 13:40:45 +0300

    adding nginx logs script

--- scripts/nginx_logs.sh
+++ scripts/nginx_logs.sh
@@ -2,7 +2,24 @@
 
 set -e
 
-log_file='/var/log/nginx/access.log'
+log_file="/var/log/nginx/access.log"
 
-echo "Top 5 ip by requests: "
+if [ ! -f "$log_file" ]; then
+    echo "log file not found"
+    exit 1
+fi
+
+echo "Top 5 ip addresses with most requests"
 awk '{print $1}' "$log_file" | sort | uniq -c | sort -nr | head -5
+
+echo -e "\nTop 5 most requested paths"
+
+awk '{print $7}' "$log_file" | sort | uniq -c | sort -nr | head -5
+
+echo -e "\n Top 5 response status codes: "
+
+awk '{print $9}' "$log_file" | sort | uniq -c | sort -nr | head -5
+
+echo -e "Top 5 user agents: "
+
+awk -F\" '{print $6}' "$log_file" | sort | uniq -c | sort -nr | head -5
